0f0f2ce00c228d6ed286767b0dd56a8f22de042a
Sheilah <sheilahndiema@gmail.com>
2022-08-05 16:16:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(listOfNames, event) {
+    const messages = []
+    for (let i = 0; i < listOfNames.length; i++) {
+        const name = listOfNames[i];
+        let message = `Thank you, ${name}, for the wonderful ${event} gift!`
+        messages.push(message)
+    }
+    return messages
+}
+function countDown(n) {
+    while (n >= 0) {
+        console.log(n)
+        --n
+    }
+}
